84c2afe5d2eaaf2e5cf0cd9848b94c3932b2be5b
Mateusz <mateusz.wenerski.pl@gmail.com>
2019-04-29 13:48:17 +0200

    TableView work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -20,24 +20,71 @@ public class Controller implements Initializable {
     @FXML private TableColumn <Reservation, String> col_surname;
     @FXML private TableColumn <Reservation, String> col_checkin;
     @FXML private TableColumn <Reservation, String> col_checkout;
-    @FXML private TableColumn <Reservation, String> col_reservationNumber;
+    @FXML private TableColumn <Reservation, Integer> col_reservationNumber;
 
+    int reservationVariable = 1;
 
     public ObservableList<Reservation> reservations_list = FXCollections.observableArrayList(
-            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", 1),
-            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", 2),
-            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", 3),
-            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", 4)
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++),
+            new Reservation(123, "John", "Johnowski", "12-05-1998", "15-05-1998", reservationVariable++),
+            new Reservation(321, "Andrzej", "Adamowski", "11-05-1998", "12-05-1998", reservationVariable++),
+            new Reservation(432, "Jola", "Jolowska", "12-06-1998", "16-06-1998", reservationVariable++),
+            new Reservation(231, "Jan", "Janowski", "06-07-1998", "15-07-1998", reservationVariable++)
+
+
     );
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
-        col_name.setCellValueFactory(new PropertyValueFactory<Reservation, String>("Name"));
-        col_surname.setCellValueFactory(new PropertyValueFactory<Reservation, String>("Surname"));
-        col_room.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("Room"));
-        col_checkin.setCellValueFactory(new PropertyValueFactory<Reservation, String>("Check In"));
-        col_checkout.setCellValueFactory(new PropertyValueFactory<Reservation, String>("Check Out"));
-        col_reservationNumber.setCellValueFactory(new PropertyValueFactory<Reservation, String>("Reservation Number"));
+        col_name.setCellValueFactory(new PropertyValueFactory<Reservation, String>("guestName"));
+        col_surname.setCellValueFactory(new PropertyValueFactory<Reservation, String>("guestSurname"));
+        col_room.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("room"));
+        col_checkin.setCellValueFactory(new PropertyValueFactory<Reservation, String>("checkInDate"));
+        col_checkout.setCellValueFactory(new PropertyValueFactory<Reservation, String>("checkOutDate"));
+        col_reservationNumber.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("reservationNumber"));
         table.setItems(reservations_list);
 
     }
--- src/sample/Reservation.java
+++ src/sample/Reservation.java
@@ -1,79 +1,116 @@
 package sample;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Reservation {
 
-    private int room;
-    private String guestName;
-    private String guestSurname;
-    private Date checkInDate;
-    private Date checkOutDate;
-    private int reservationNumber;
+    private  SimpleIntegerProperty room;
+    private  SimpleStringProperty guestName;
+    private  SimpleStringProperty guestSurname;
+    private  SimpleStringProperty checkInDate;
+    private  SimpleStringProperty checkOutDate;
+    private  SimpleIntegerProperty reservationNumber;
 
-    public Reservation(int room, String guestName, String guestSurname, String checkInDateString, String checkOutDateString, int reservationNumber) {
+    public Reservation(SimpleIntegerProperty room, SimpleStringProperty guestName, SimpleStringProperty guestSurname, SimpleStringProperty checkInDate, SimpleStringProperty checkOutDate, SimpleIntegerProperty reservationNumber) {
         this.room = room;
         this.guestName = guestName;
         this.guestSurname = guestSurname;
-        this.reservationNumber = reservationNumber;
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy"); //formater is necessary to format from String to Date, bacause Java can't take Date format in constructor - in this case, only String
-        try {
-            Date checkInDate = formatter.parse(checkInDateString);
-            Date checkOutDate = formatter.parse(checkOutDateString);
-        } catch (ParseException e) {e.printStackTrace();}
-
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
+        this.reservationNumber = reservationNumber;
+        
     }
 
+    public Reservation(Integer room, String guestName, String guestSurname, String checkInDate, String checkOutDate, Integer reservationNumber) {
+        this.room = new SimpleIntegerProperty(room);
+        this.guestName = new SimpleStringProperty(guestName);
+        this.guestSurname = new SimpleStringProperty(guestSurname);
+        this.checkInDate = new SimpleStringProperty(checkInDate);
+        this.checkOutDate = new SimpleStringProperty(checkOutDate);
+        this.reservationNumber = new SimpleIntegerProperty(reservationNumber);
+    }
+
+    //        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy"); //formater is necessary to format from String to Date, bacause Java can't take Date format in constructor - in this case, only String
+//        try {
+//            Date checkInDate = formatter.parse(checkInDateString);
+//            Date checkOutDate = formatter.parse(checkOutDateString);
+//        } catch (ParseException e) {e.printStackTrace();}
+
+
     public int getRoom() {
+        return room.get();
+    }
+
+    public SimpleIntegerProperty roomProperty() {
         return room;
     }
 
     public void setRoom(int room) {
-        this.room = room;
+        this.room.set(room);
     }
 
     public String getGuestName() {
+        return guestName.get();
+    }
+
+    public SimpleStringProperty guestNameProperty() {
         return guestName;
     }
 
     public void setGuestName(String guestName) {
-        this.guestName = guestName;
+        this.guestName.set(guestName);
     }
 
     public String getGuestSurname() {
+        return guestSurname.get();
+    }
+
+    public SimpleStringProperty guestSurnameProperty() {
         return guestSurname;
     }
 
     public void setGuestSurname(String guestSurname) {
-        this.guestSurname = guestSurname;
+        this.guestSurname.set(guestSurname);
     }
 
-    public Date getCheckInDate() {
+    public String getCheckInDate() {
+        return checkInDate.get();
+    }
+
+    public SimpleStringProperty checkInDateProperty() {
         return checkInDate;
     }
 
-    public void setCheckInDate(Date checkInDate) {
-        this.checkInDate = checkInDate;
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate.set(checkInDate);
     }
 
-    public Date getCheckOutDate() {
+    public String getCheckOutDate() {
+        return checkOutDate.get();
+    }
+
+    public SimpleStringProperty checkOutDateProperty() {
         return checkOutDate;
     }
 
-    public void setCheckOutDate(Date checkOutDate) {
-        this.checkOutDate = checkOutDate;
+    public void setCheckOutDate(String checkOutDate) {
+        this.checkOutDate.set(checkOutDate);
     }
 
     public int getReservationNumber() {
+        return reservationNumber.get();
+    }
+
+    public SimpleIntegerProperty reservationNumberProperty() {
         return reservationNumber;
     }
 
     public void setReservationNumber(int reservationNumber) {
-        this.reservationNumber = reservationNumber;
+        this.reservationNumber.set(reservationNumber);
     }
 }
